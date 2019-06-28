0f9b069f5b6523634059636a6baddf63eb4a7e66
luanYuCheng <825946752@qq.com>
2019-06-28 11:46:19 +0800

    登录页面基础页面完成

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import elementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "./assets/css/index.css";
 
+Vue.use(elementUI);
 Vue.config.productionTip = false;
 
 new Vue({

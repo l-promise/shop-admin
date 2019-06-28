bc6daa59135cdf88bbcca01a72f31ac8418269b1
luanYuCheng <825946752@qq.com>
2019-06-28 16:04:39 +0800

    登录页基本完成

--- src/views/login.vue
+++ src/views/login.vue
@@ -2,21 +2,24 @@
   <el-row type="flex" class="row-bg" justify="center" align="middle">
     <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
       <el-form
-        ref="form"
+        ref="loginForm"
+        :rules="loginRules"
         :model="form"
         class="login-form"
         label-width="80px"
         label-position="top"
       >
-        <el-form-item label="用户名">
-          <el-input v-model="form.name"></el-input>
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="form.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
-          <el-input v-model="form.name"></el-input>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="form.password"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">登录</el-button>
-          <el-button>重置</el-button>
+          <el-button type="primary" @click="submitForm('loginForm')"
+            >登录</el-button
+          >
+          <el-button @click="resetForm('loginForm')">重置</el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -24,23 +27,52 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       form: {
-        name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: ""
+        username: "",
+        password: ""
+      },
+      loginRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 5, max: 8, message: "长度在 5 到 8 个字符", trigger: "change" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 12,
+            message: "长度在 6 到 12 个字符",
+            trigger: "change"
+          }
+        ]
       }
     };
   },
   methods: {
-    onSubmit() {}
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          axios({
+            url: "http://localhost:8888/api/private/v1/login",
+            method: "post",
+            data: this.form
+          }).then(({ data: { data, meta } }) => {
+            if (meta.status == 200) {
+              this.$router.puth("/home");
+            }
+          });
+        } else {
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
   }
 };
 </script>
